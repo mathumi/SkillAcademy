f2032dfcf5bc9d59930a5930838ccbb5ac7e0521
Pavithra <pavithra.b@payoda.com>
2018-06-16 20:02:59 +0530

    Home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Nav, Platform } from 'ionic-angular';
+import { ListPage } from '../list/list';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @ViewChild(Nav) nav: Nav;
+  pages: Array<{title: string, component: any}> =[
+    { title: 'List', component: ListPage }
+];
 
   constructor(public navCtrl: NavController) {
-
+  }
+  openPage(page) {
+    console.log(page);
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.navCtrl.setRoot("ListPage");
   }
 
 }
